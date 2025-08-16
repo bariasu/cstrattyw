b7f482ef63e5343d01bb4480abaca13f03cb5856
Miepee <janbidler00@protonmail.com>
2025-08-16 02:35:31 +0200

    Convert to spaces, wrap strings, get rid of decorator, simplify add, add editorconfig

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import cstrattyw
 
-if __name__ =='__main__':
-
+if __name__ == '__main__':
     with open("token.txt", "r") as f:
         token = f.readlines()[0].strip()
 
-    cstrattyw.runBot(token)
+    cstrattyw.run_bot(token)
